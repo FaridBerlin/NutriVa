7bb2bfc457ed643662a5ef314764889e4d3b1571
Farid <bughunterf@gmail.com>
2026-01-31 22:54:41 +0100

    make AI Doctor page mobile responsive

--- frontend/src/pages/AiDoctorPage.jsx
+++ frontend/src/pages/AiDoctorPage.jsx
@@ -3,9 +3,9 @@ import AiDoctor from '../components/AiDoctor'
 
 export default function AiDoctorPage() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <Sidebar />
-      <main className="ml-64 p-4">
+      <main className="lg:ml-64 p-2 sm:p-4 pt-16 lg:pt-4">
         <AiDoctor />
       </main>
     </div>
